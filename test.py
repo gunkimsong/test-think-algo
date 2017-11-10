28ecce45a5422dd1d88112bb08bdb9b069b997c2
gunkimsong <gunkimsong8@gmail.com>
2017-11-10 15:27:30 +0700

    + Split by time

--- test.py
+++ test.py
@@ -2,118 +2,151 @@ import json
 import time
 import csv
 
-
-def divide(quantity):
-    with open('settings.json') as settings_data:
-        settings = json.load(settings_data)
-    if 100 <= abs(quantity) <= 1000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['1k'])
-        else:
-            return quantity/int(settings['divide']['1k']-1)
-    if 1001 <= abs(quantity) <= 3000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['3k'])
-        else:
-            return quantity/int(settings['divide']['3k']-1)
-    if 3001 <= abs(quantity) <= 6000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['6k'])
-        else:
-            return quantity/int(settings['divide']['6k']-1)
-    if 6001 <= abs(quantity) <= 8000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['8k'])
-        else:
-            return quantity/int(settings['divide']['8k']-1)
-    if abs(quantity) >= 8001:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['10k'])
-        else:
-            return quantity/int(settings['divide']['10k']-1)
-
-
-def divide_yesterday(quantity):
-    with open('settings.json') as settings_data:
-        settings = json.load(settings_data)
-    if 100 <= abs(quantity) <= 1000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['1k'])
-        else:
-            return quantity/int(settings['divide']['1k']-1)
-    if 1001 <= abs(quantity) <= 3000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['1k'])
-        else:
-            return quantity/int(settings['divide']['1k']-1)
-    if 3001 <= abs(quantity) <= 6000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['3k'])
-        else:
-            return quantity/int(settings['divide']['3k']-1)
-    if 6001 <= abs(quantity) <= 8000:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['6k'])
-        else:
-            return quantity/int(settings['divide']['6k']-1)
-    if abs(quantity) >= 8001:
-        if quantity % 2 == 0:
-            return quantity/int(settings['divide']['8k'])
-        else:
-            return quantity/int(settings['divide']['8k']-1)
-
 data = []
-
 # Read File
 try:
     with open('data.json') as json_data:
         d = json.load(json_data)
+    with open('settings.json') as settings_data:
+        settings = json.load(settings_data)
 except EnvironmentError:
     print('JSON File error')
-else:
-    for json_data in d:
-        yesterday_flag = False
-        if abs(json_data['quantity']) > 100:
-            dtime = str(time.strptime(json_data['date'], "%d/%m/%Y").tm_mday-1) + '/' + \
-                    str(time.strptime(json_data['date'], "%d/%m/%Y").tm_mon) + '/' + \
-                    str(time.strptime(json_data['date'], "%d/%m/%Y").tm_year)
 
-            yesterday_symbol = (x for x in d if x['date'] == dtime and x['symbol'] == json_data['symbol'])
 
-            # Split Quantity
-            json_data['amt'] = divide(json_data['quantity'])
-            json_data['split'] = int(json_data['quantity']/json_data['amt'])
+def divide(quantity):
+    arg = []
+    if 100 <= abs(quantity) <= 1000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['1k']))
+            arg.append(int(settings['divide']['1k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['1k']-1))
+            arg.append(int(settings['divide']['1k']-1))
+            return arg
+    if 1001 <= abs(quantity) <= 3000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['3k']))
+            arg.append(int(settings['divide']['3k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['3k']-1))
+            arg.append(int(settings['divide']['3k']-1))
+            return arg
+    if 3001 <= abs(quantity) <= 6000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['6k']))
+            arg.append(int(settings['divide']['6k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['6k']-1))
+            arg.append(int(settings['divide']['6k']-1))
+            return arg
+    if 6001 <= abs(quantity) <= 8000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['8k']))
+            arg.append(int(settings['divide']['8k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['8k']-1))
+            arg.append(int(settings['divide']['8k']-1))
+            return arg
+    if abs(quantity) >= 8001:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['10k']))
+            arg.append(int(settings['divide']['10k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['10k']-1))
+            arg.append(int(settings['divide']['10k']-1))
+            return arg
 
-            for x in yesterday_symbol:
-                if yesterday_flag:
-                    break
-                else:
-                    # If Symbol exist in yesterday , Do Lower Split.
-                    yesterday_flag = True
-                    json_data['amt'] = divide_yesterday(json_data['quantity'])
-                    json_data['split'] = int(json_data['quantity']/json_data['amt'])
 
-            data.append(json_data)
+def divide_yesterday(quantity):
 
-            # Print Data
-            print(json_data)
+    arg = []
+    if 100 <= abs(quantity) <= 1000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['1k']))
+            arg.append(int(settings['divide']['1k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['1k']-1))
+            arg.append(int(settings['divide']['1k'])-1)
+            return arg
+    if 1001 <= abs(quantity) <= 3000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['1k']))
+            arg.append(int(settings['divide']['1k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['1k']-1))
+            arg.append(int(settings['divide']['1k']-1))
+            return arg
+    if 3001 <= abs(quantity) <= 6000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['3k']))
+            arg.append(int(settings['divide']['3k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['3k']-1))
+            arg.append(int(settings['divide']['3k']-1))
+            return arg
+    if 6001 <= abs(quantity) <= 8000:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['6k']))
+            arg.append(int(settings['divide']['6k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['6k']-1))
+            arg.append(int(settings['divide']['6k']-1))
+            return arg
+    if abs(quantity) >= 8001:
+        if quantity % 2 == 0:
+            arg.append(quantity/int(settings['divide']['8k']))
+            arg.append(int(settings['divide']['8k']))
+            return arg
+        else:
+            arg.append(quantity/int(settings['divide']['8k']-1))
+            arg.append(int(settings['divide']['8k']-1))
+            return arg
 
-# Enter Date to Export CSV
-date_export = input('Enter Date to Export CSV (DD/MM/YYYY format) * Leave blank to get all : ')
 
-with open('Data'+date_export+'.csv', 'w') as csvfile:
-    fieldnames = ['date', 'account', 'symbol', 'quantity', 'split', 'amt']
+for json_data in d:
+    yesterday_flag = False
+    if abs(json_data['quantity']) > 100:
+        dtime = str(time.strptime(json_data['date'], "%d/%m/%Y").tm_mday-1) + '/' + \
+                str(time.strptime(json_data['date'], "%d/%m/%Y").tm_mon) + '/' + \
+                str(time.strptime(json_data['date'], "%d/%m/%Y").tm_year)
+
+        yesterday_symbol = (x for x in d if x['date'] == dtime and x['symbol'] == json_data['symbol'])
+
+        for x in yesterday_symbol:
+            if yesterday_flag:
+                break
+            else:
+                yesterday_flag = True
+
+        if yesterday_flag:
+            split_arg = divide(json_data['quantity'])
+        else:
+            split_arg = divide_yesterday(json_data['quantity'])
+
+        for num in range(0, split_arg[1]):
+            tmp_data = {}
+            tmp_data['date'] = json_data['date']
+            tmp_data['time'] = settings['timeslot'][str(split_arg[1])][num]
+            tmp_data['account'] = json_data['account']
+            tmp_data['symbol'] = json_data['symbol']
+            tmp_data['quantity'] = split_arg[0]
+            data.append(tmp_data)
+
+
+with open('Data.csv', 'w') as csvfile:
+    fieldnames = ['date', 'time', 'account', 'symbol', 'quantity']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
 
-# If Not blank fill csv by Date
-    if date_export:
-        for item in data:
-            if item['date'] == date_export:
-                writer.writerow({'date': item['date'], 'account': item['account'], 'symbol': item['symbol'],
-                                 'quantity': item['quantity'], 'split': item['split'], 'amt': item['amt']})
-# If Blank Fill All
-    else:
-        for item in data:
-            writer.writerow({'date': item['date'], 'account': item['account'], 'symbol': item['symbol'],
-                                 'quantity': item['quantity'], 'split': item['split'], 'amt': item['amt']})
+    for item in data:
+        writer.writerow({'date': item['date'], 'time': item['time'], 'account': item['account'], 'symbol': item['symbol'],
+                             'quantity': item['quantity']})
